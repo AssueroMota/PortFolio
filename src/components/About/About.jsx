65f8e5f1319ca589878a01ad7cd238edcd6ba44f
AssueroMota <assueromota@hotmail.com>
2023-09-25 13:08:19 -0300

    edited things

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './About.css'
+
+import Typewriter from 'typewriter-effect';
 
 const About = () => {
 
@@ -12,16 +14,29 @@ const About = () => {
         <>
             <div className="About-container" data-aos="fade-up" data-aos-duration="1000">
                 <p className="About-title">Technology is the driving force behind constant change</p>
-                <h1 className="About-Name">Hi, I'm <span className="About-Name-span">Assuero Mota</span>, Specialist in</h1>
-          
-                    {/* <div class="About-Dev-container">
+                <h1 className="About-Name">Hi, I'm <span className="About-Name-span">Assuero Mota</span>, Knowledgeable in</h1>
+
+                {/* <div class="About-Dev-container">
                     <span className="About-Dev">Desenvolvimento web</span>
                     <span className="About-Dev">Desenvolvimento web</span>
                     <span className="About-Dev">Desenvolvimento web</span>
                 </div> */}
-                <p className="About-Dev">{texts[currentTextCounter]}</p>
-                
-            </div>
+
+                {/* <p className="About-Dev">
+                        {texts[currentTextCounter]}
+                    </p> */}
+                <p className="About-Dev">
+                    <Typewriter
+                        options={{
+                            strings: ["Full Stack Engineer", "Mobile Developer", "Data Analyst","I.T Analyst","Network Technician","Innovation-driven IT"],
+                            autoStart: true, 
+                            loop: true, 
+                            delay: 75, 
+                        }}
+                    />
+
+                </p>
+            </div >
         </>
     )
 }
